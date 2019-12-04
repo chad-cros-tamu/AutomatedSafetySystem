1708c6bb1e7125bfe2b6fd134e4e5721aade1fda
javila007 <jaime.avila.h@gmail.com>
2019-12-04 17:33:16 -0500

    resubmit

--- webpage/sensor/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-#makes the sensor name sensor
-class SensorConfig(AppConfig):
-    name = 'sensor'
-#make the proximity sensor name proxsensor
-class ProxConfig(AppConfig):
-	name = 'proxsensor'
--- webpage/sensorWorker/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SensorworkerConfig(AppConfig):
-    name = 'sensorWorker'
